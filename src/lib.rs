66f04fe36f4cc760ec73c60296d9a3f4bd55b65c
LukeJohnson-io <lukejohnson10@suumail.net>
2022-10-20 19:51:14 -0600

    Added comb and binomial dist

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,45 @@
+use std::cmp::min;
+
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+
+#[pyclass]
+struct Binomial {
+    trials: u128,
+    prob: f64,
+}
+
+#[pymethods]
+impl Binomial {
+    #[new]
+    fn new(trials: u128, prob: f64) -> Self {
+        Self {
+            trials: trials,
+            prob: prob,
+        }
+    }
+
+    fn dist(&self, successes: u128) -> f64 {
+        comb(self.trials, successes)
+        * self.prob.powi(successes as i32)
+        * (1.0_f64 - self.prob).powi((self.trials - successes) as i32)
+    }
+}
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn comb(n: u128, k: u128) -> f64 {
+    if n == k {
+        return 1.0;
+    }
+    let upper: u128 = min(k, n-k);
+    let pool = n as f64;
+    (1..=upper).into_iter().map(|i| (pool + 1.0 - i as f64)/(i as f64)).product::<f64>()
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn restpy(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(comb, m)?)?;
+    m.add_class::<Binomial>()?;
     Ok(())
 }
